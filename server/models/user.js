5353c30cdb47b85bfe49291182bcb5d3cedcf218
blarryy <34322953+blarryy@users.noreply.github.com>
2018-03-23 10:11:02 -0700

    add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -46,16 +46,25 @@ UserSchema.methods.generateAuthToken = function () {
   var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
   user.tokens = user.tokens.concat([{
- access: access,
- token: token
-}]);
+   access: access,
+   token: token
+  }]);
 
   return user.save().then(() => {
     return token;
   });
 };
-//static method: just like java. don't need an Object
-//to call a  static method. Just pass in something
+
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function (token) {
   var User = this;
   var decoded;
@@ -73,30 +82,26 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
-  UserSchema.statics.findByCredentials = function (email, password) {
-    var User = this;
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
 
-    return User.findOne({email}).then((user) => {
-      if (!user){
-        return Promise.reject();
-      }
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
 
-      return new Promise((resolve, reject) => {
-        bcrypt.compare(password, user.password, (err, res) => {
-          if (res) {
-            resolve(user);
-          }else {
-            reject();
-          }
-        });
+    return new Promise((resolve, reject) => {
+      // Use bcrypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
       });
     });
-  };
-
-
-
-
-
+  });
+};
 
 UserSchema.pre('save', function (next) {
   var user = this;
--- server/server.js
+++ server/server.js
@@ -85,7 +85,7 @@ app.patch('/todos/:id', (req, res) => {
     body.completed = false;
     body.completedAt = null;
   }
-``
+
   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
     if (!todo) {
       return res.status(404).send();
@@ -114,15 +114,12 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
-//we used the same salt "123abc" on token.
-//used different salts on password
 
-//POST /users/login
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
   User.findByCredentials(body.email, body.password).then((user) => {
-    user.generateAuthToken().then((token) => {
+    return user.generateAuthToken().then((token) => {
       res.header('x-auth', token).send(user);
     });
   }).catch((e) => {
@@ -130,21 +127,13 @@ app.post('/users/login', (req, res) => {
   });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
